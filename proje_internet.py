9667361df14b78664c2e2f939b12c7bb09b66db1
YgtEfeDvcglu <deveciogluyigitefe@gmail.com>
2026-06-01 17:55:05 +0300

    Update proje_internet.py

--- proje_internet.py
+++ proje_internet.py
@@ -19,7 +19,8 @@ st.set_page_config(page_title="Gök Mekaniği Raporlayıcı", layout="centered",
 st.sidebar.title("Proje Hakkında")
 st.sidebar.markdown("---")
 st.sidebar.caption("© 2026 Tüm Hakları Saklıdır.\n"
-                   "Yiğit Efe Devecioğlu\n\n"
+                   "Yiğit Efe Devecioğlu\n"
+                   "deveciogluyigitefe@gmail.com\n\n"
                    "Bu araç, yörünge dinamikleri hesaplamalarını otomatize "
                    "etmek amacıyla geliştirilmiş açık kaynaklı bir projedir.")
 
